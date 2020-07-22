42bfbc342cb3c0106b73d1886194b37e845ab3bc
akida <47895118+Akida31@users.noreply.github.com>
2020-07-22 23:04:29 +0200

    first version

---
+++ src/hashing.rs
@@ -0,0 +1,139 @@
+use std::fmt::Write;
+use std::fs::File;
+use std::io::{BufReader, Read};
+use std::path::PathBuf;
+
+use sha1::Digest;
+use std::fs;
+
+/// The size of the buffer reading the file
+const BUFFER_SIZE: usize = 1024;
+
+/// Get a List of all supported algorithms
+pub fn available_algorithms() -> Vec<String> {
+    vec!["md5", "sha1", "sha256", "sha384", "sha512"]
+        .into_iter()
+        .map(|s| s.to_string())
+        .collect()
+}
+
+/// Evaluates the algorithm from the length of a hash
+///
+/// # Examples
+/// ```
+/// assert_eq!(get_algorithm_by_hash_len(32), Some(String::from("md5"))
+/// assert_eq!(get_algorithm_by_hash_len(17), None)
+/// ```
+pub fn get_algorithm_by_hash_len(len: usize) -> Option<String> {
+    Some(
+        match len {
+            32 => "md5",
+            40 => "sha1",
+            64 => "sha256",
+            96 => "sha384",
+            128 => "sha512",
+            _ => return None,
+        }
+        .to_string(),
+    )
+}
+
+/// Converts a byte-sequence to a human readable format.
+///
+/// The output consists of lowercase hexadecimal numbers.
+///
+/// # Examples
+/// ```
+/// // some bytes in a array
+/// let input: &[u8] = &[240, 159, 146, 32];
+/// assert_eq!(byte_to_hex(input), String::from("hiasdf"))
+/// ```
+fn byte_to_hex(input: &[u8]) -> String {
+    let mut out = String::new();
+    for &byte in input {
+        write!(&mut out, "{:x}", byte).expect("Parsing Error");
+    }
+    out
+}
+
+/// generator for the match expression of the different algorithms
+/// # Examples
+/// ```
+/// let algorithm = "md5";
+/// let mut hasher: Box<dyn digest::DynDigest> = match_algo!(algorithm, ["md5" => md5::Md5, "sha1" => sha1::Sha1]);
+/// ```
+macro_rules! generate_hasher {
+    ($matcher: expr, [$($name: expr => $func: ty),+]) => {{
+        match $matcher {
+            $($name => {{
+                Box::new(<$func>::new())
+            }},)+
+            a => unimplemented!("Algorithm {} is not implemented", a)
+        }
+    }}
+}
+
+// TODO document this
+fn get_files_of_directory(direcory: PathBuf) -> std::io::Result<Vec<PathBuf>> {
+    let mut result: Vec<PathBuf> = Vec::new();
+    for entry in fs::read_dir(direcory)? {
+        let path = entry?.path();
+        if path.is_dir() {
+            result.append(&mut get_files_of_directory(path).unwrap());
+        } else {
+            result.push(path);
+        }
+    }
+    Ok(result)
+}
+
+/// computes the hash of the given path with the given algorithm
+///
+///
+/// returns the hexdigits (lowercase) of the hash
+/// Path must be a valid path.
+/// If the Path is a directory, all subdirecories with their files will be hashed
+///
+/// # Errors:
+/// if `path` is not a valid path
+/// if `algorithm` is not a supported algorithm, get a list of all by calling `available_algorithms`
+///
+/// # Example:
+/// hash a file
+/// ```
+/// use std::path::Pathbuf;
+/// let file = PathBuf::from("./Cargo.toml");
+/// let algorithm = "sha256";
+/// let result = hash(file, &algorithm);
+/// ```
+pub fn hash(path: PathBuf, algorithm: &str) -> Result<String, String> {
+    if !available_algorithms().contains(&algorithm.to_string()) {
+        return Err(format!("{} is not a available algorithm!", algorithm));
+    }
+    let mut hasher: Box<dyn digest::DynDigest> = generate_hasher!(algorithm, [
+            "md5" => md5::Md5,
+            "sha1" => sha1::Sha1,
+            "sha256" => sha2::Sha256,
+            "sha384" => sha2::Sha384,
+            "sha512" => sha2::Sha512]);
+    let files = if path.is_file() {
+        vec![path]
+    } else {
+        get_files_of_directory(path).unwrap()
+    };
+    for file in files {
+        let file = match File::open(&file) {
+            Ok(f) => f,
+            Err(_) => return Err(format!("{:?} is not a valid path!", file)),
+        };
+        let mut reader = BufReader::new(file);
+        let mut buffer = [0u8; BUFFER_SIZE];
+        while let Ok(n) = reader.read(&mut buffer) {
+            hasher.update(&buffer[..n]);
+            if n == 0 || n < BUFFER_SIZE {
+                break;
+            }
+        }
+    }
+    Ok(byte_to_hex(&*hasher.finalize()))
+}
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,28 @@
-mod hash;
-
-use crate::hash::{algorithm_available, get_algorithm, hash};
+use crate::hashing::get_algorithm_by_hash_len;
 use std::io::Write;
-use std::path::Path;
+use std::path::PathBuf;
 
-fn main() {
+mod hashing;
+
+fn get_path() -> PathBuf {
     let mut input = String::new();
-    let file = loop {
+    loop {
         print!("Please put in the path: ");
         std::io::stdout().flush().expect("Output error");
         std::io::stdin()
             .read_line(&mut input)
             .expect("Can't read input");
         input = input.trim().parse().unwrap();
-        let path = Path::new(&input);
-        if path.is_file() {
+        let path = PathBuf::from(&input);
+        if path.is_file() || path.is_dir() {
             break path;
-        }
-        else {
+        } else {
             println!("path {:?} not found", path);
         }
-    };
+    }
+}
+
+fn get_algorithm() -> (String, String) {
     let mut input = String::new();
     print!("Please put in the hash or leave it empty to compute the hash only: ");
     std::io::stdout().flush().expect("Output error");
@@ -28,33 +30,37 @@ fn main() {
         .read_line(&mut input)
         .expect("Can't read input");
     let given_hash: String = input.trim().parse().unwrap();
-    let mut input = String::new();
-    let algorithm = match get_algorithm(given_hash.clone()) {
-        Some(a) => a,
+    match get_algorithm_by_hash_len(given_hash.len()) {
+        Some(a) => (a, given_hash),
         None => loop {
             print!("Please put in the algorithm: ");
             std::io::stdout().flush().expect("Output error");
             std::io::stdin()
                 .read_line(&mut input)
                 .expect("Can't read input");
-            let input: String = input.trim().parse().unwrap();
-            if algorithm_available(input.clone()) {
-                break input;
-            }
-            else {
-                println!("Algorithm not available");
-                // TODO print available algorithms
+            let algorithm: String = input.trim().parse().unwrap();
+            if hashing::available_algorithms().contains(&algorithm) {
+                break (algorithm, given_hash);
+            } else {
+                println!("Algorithm not available\nAvailable algorithms:");
+                for algo in hashing::available_algorithms() {
+                    println!("  {}", algo);
+                }
             }
         },
-    };
-    let computed_hash = hash(file, algorithm);
+    }
+}
+
+fn main() {
+    // TODO cli with args
+    let file = get_path();
+    let (algorithm, given_hash) = get_algorithm();
+    let computed_hash = hashing::hash(file, &*algorithm).unwrap();
     if given_hash.len() < 2 {
         println!("{}", computed_hash);
+    } else if computed_hash == given_hash {
+        println!("The hashes are equal");
     } else {
-        if computed_hash == given_hash {
-            println!("The hashes are equal");
-        } else {
-            println!("ERROR\nThe hashes are NOT equal");
-        }
+        println!("ERROR\nThe hashes are NOT equal");
     }
 }
